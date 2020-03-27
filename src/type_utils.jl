6c61028b6f571c2da644c4ad5fed7ae9d22092ca
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-03-27 19:02:56 +0000

    fix missing backticks

--- src/type_utils.jl
+++ src/type_utils.jl
@@ -6,6 +6,7 @@ Extracts the type-parameters of the `type`.
 ```jldoctest
 julia> parameters(Foo{A, B, C}) == [A, B, C]
 true
+```
 """
 parameters(sig::UnionAll) = parameters(sig.body)
 parameters(sig::DataType) = sig.parameters
